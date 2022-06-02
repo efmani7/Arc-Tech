18000dcf3af5c6b4513fcf25d347bea2cf728c7b
efmani7 <106765445+efmani7@users.noreply.github.com>
2022-06-02 13:37:57 -0400

    Webfiles

---
+++ Arc_Project/samsung.js
@@ -0,0 +1,68 @@
+let imgtopleft = document.querySelector('#toplefts22');
+let imgtopright = document.querySelector('#toprights22');
+let imgbottom = document.querySelector('#bottomcams22');
+let buttongreen =document.querySelector('#buttongreens22');
+let buttonpink =document.querySelector('#buttonpinks22');
+let buttonwhite =document.querySelector('#buttonwhites22');
+let buttonblack =document.querySelector('#buttonblacks22');
+
+
+buttonblack.addEventListener('click', ()=>{
+  imgtopleft.src='samsung/phones/s22backblack.png'
+  imgtopright.src='samsung/phones/s22angledblack.png'
+  imgbottom.src='samsung/phones/s22blackcam.png'
+})
+
+buttongreen.addEventListener('click', ()=>{
+  imgtopleft.src='samsung/phones/s22backgreen.png'
+  imgtopright.src='samsung/phones/s22angledgreen.png'
+  imgbottom.src='samsung/phones/s22greencam.png'
+})
+
+buttonwhite.addEventListener('click', ()=>{
+  imgtopleft.src='samsung/phones/s22backwhite.png'
+  imgtopright.src='samsung/phones/s22angledwhite.png'
+  imgbottom.src='samsung/phones/s22whitecam.png'
+})
+
+buttonpink.addEventListener('click', ()=>{
+  imgtopleft.src='samsung/phones/s22backpink.png'
+  imgtopright.src='samsung/phones/s22angledpink.png'
+  imgbottom.src='samsung/phones/s22pinkcam.png'
+})
+
+/* S22 ULTRA STYLES*/
+
+
+let imgtopleftultra = document.querySelector('#toplefts22ultra');
+let imgtoprightultra = document.querySelector('#toprights22ultra');
+let imgbottomultra = document.querySelector('#bottomcams22ultra');
+let buttongreenultra =document.querySelector('#buttongreens22ultra');
+let buttonpinkultra =document.querySelector('#buttonpinks22ultra');
+let buttonwhiteultra =document.querySelector('#buttonwhites22ultra');
+let buttonblackultra =document.querySelector('#buttonblacks22ultra');
+
+
+buttonblackultra.addEventListener('click', ()=>{
+  imgtopleftultra.src='samsung/phones/s22ultrabackblack.png'
+  imgtoprightultra.src='samsung/phones/s22ultraangledblack.png'
+  imgbottomultra.src='samsung/phones/s22blackcam.png'
+})
+
+buttongreenultra.addEventListener('click', ()=>{
+  imgtopleftultra.src='samsung/phones/s22ultrabackgreen.png'
+  imgtoprightultra.src='samsung/phones/s22ultraangledgreen.png'
+  imgbottomultra.src='samsung/phones/s22ultragreencam.png'
+})
+
+buttonwhiteultra.addEventListener('click', ()=>{
+  imgtopleftultra.src='samsung/phones/s22ultrabackwhite.png'
+  imgtoprightultra.src='samsung/phones/s22ultraangledwhite.png'
+  imgbottomultra.src='samsung/phones/s22ultrawhitecam.png'
+})
+
+buttonpinkultra.addEventListener('click', ()=>{
+  imgtopleftultra.src='samsung/phones/s22ultrabackburgundy.png'
+  imgtoprightultra.src='samsung/phones/s22ultraangledburgundy.png'
+  imgbottomultra.src='samsung/phones/s22ultraburgundycam.png'
+})
